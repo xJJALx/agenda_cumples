ec6e6e8745fdb99976161331eb6ca327408eeaa7
Jhon <jhonjairookawaii@gmail.com>
2022-05-23 20:26:39 +0200

    Fuente color negro tema light

--- lib/ui/themes/light_theme.dart
+++ lib/ui/themes/light_theme.dart
@@ -8,14 +8,14 @@ class LightTheme {
       secondary: const Color(0xFFf4f2fe),
     ),
     textTheme: TextTheme(
-      headline1: GoogleFonts.play(fontSize: 44),
-      headline2: GoogleFonts.play(fontSize: 34),
-      headline3: GoogleFonts.play(fontSize: 24),
-      headline4: GoogleFonts.play(fontSize: 18, fontWeight: FontWeight.w500),
+      headline1: GoogleFonts.play(fontSize: 44, color: const Color.fromARGB(211, 0, 0, 0)),
+      headline2: GoogleFonts.play(fontSize: 34, color: const Color.fromARGB(211, 0, 0, 0)),
+      headline3: GoogleFonts.play(fontSize: 24,color: const Color.fromARGB(211, 0, 0, 0)),
+      headline4: GoogleFonts.play(fontSize: 18, color: const Color.fromARGB(211, 0, 0, 0),fontWeight: FontWeight.w500),
       subtitle1: const TextStyle(fontSize: 16, color: Color.fromARGB(213, 122, 122, 122)),
-      labelMedium: const TextStyle(fontSize: 14),
-      button: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+      labelMedium: const TextStyle(fontSize: 14,color:  Color.fromARGB(211, 0, 0, 0)),
+      button: const TextStyle(fontSize: 14, color:  Color.fromARGB(211, 0, 0, 0), fontWeight: FontWeight.bold),
     ),
-    iconTheme: const IconThemeData(color:  Colors.black87),
+    iconTheme: const IconThemeData(color:  Color.fromARGB(211, 0, 0, 0)),
   );
 }
